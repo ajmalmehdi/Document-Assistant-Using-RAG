39543b45a3f2820b5eb2d5005c2fd49cea38a729
ajmalmehdi <ajmalmehdi07@gmail.com>
2024-12-11 01:49:47 +0100

    last commit

--- ollama/ollama.sh
+++ ollama/ollama.sh
@@ -1,17 +1,13 @@
-#!/bin/bash
-
 ./bin/ollama serve &
+
 pid=$!
 
-# Check if Ollama is ready (replace with a more robust check if needed)
-while ! nc -z localhost 11434 ; do
-    sleep 1
-    echo "Waiting for Ollama to start..."
-done
+sleep 5
+
 
 echo "Pulling models"
-ollama pull gemma2 || { echo "Error pulling gemma2"; exit 1; }
-ollama pull mxbai-embed-large || { echo "Error pulling mxbai-embed-large"; exit 1; }
+ollama pull gemma2
+ollama pull mxbai-embed-large
+
 
 wait $pid
-exit $? 
